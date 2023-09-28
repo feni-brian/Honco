c79a0ea130fd2fea60748693ca0f016962de7698
Feni Brian <56452538+feni-brian@users.noreply.github.com>
2023-09-28 17:39:19 +0300

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline, ThemeProvider } from "@mui/material";
+// import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { ColourModeContext, useMode } from "./theme";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [theme, colourMode] = useMode();
+	return (
+		<ColourModeContext.Provider value={colourMode}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<div className="app">
+          <></>
+					<main className="content">
+            <></>
+						<Routes>
+							<Route path="/" element={<></>} />
+							<Route path="/team" element={<></>} />
+							<Route path="/contacts" element={<></>} />
+							<Route path="/invoices" element={<></>} />
+							<Route path="/form" element={<></>} />
+							<Route path="/bar" element={<></>} />
+							<Route path="/pie" element={<></>} />
+							<Route path="/line" element={<></>} />
+							<Route path="/faq" element={<></>} />
+							<Route path="/calendar" element={<></>} />
+							<Route path="/geography" element={<></>} />
+						</Routes>
+					</main>
+				</div>
+			</ThemeProvider>
+		</ColourModeContext.Provider>
+	);
 }
 
 export default App;
