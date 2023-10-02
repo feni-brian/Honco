0685a2af9a43f1d33fe6d33a5e94cba49925cf18
Feni Brian <56452538+feni-brian@users.noreply.github.com>
2023-10-02 21:30:22 +0300

    Added configurations for the tooltip.

--- src/components/pie-chart.jsx
+++ src/components/pie-chart.jsx
@@ -20,6 +20,7 @@ const PieChart = () => {
 					},
 				},
 				legends: { text: { fill: colours.grey[100] } },
+				tooltip: { container: { background: colours.grey[600] } },
 			}}
 			margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
 			innerRadius={0.5}
